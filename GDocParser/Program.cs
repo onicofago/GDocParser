38809a3d3d616d18f4e682bddde464660f4aaf1d
drobledobw <diego.robledo@tarmac.io>
2019-11-22 15:47:00 -0300

    Add project files.

---
+++ GDocParser/Program.cs
@@ -0,0 +1,129 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Docs.v1;
+using Google.Apis.Docs.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+namespace DocsQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/docs.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { DocsService.Scope.DocumentsReadonly };
+        static string ApplicationName = "Google Docs API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Docs API service.
+            var service = new DocsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String documentId = "10iaemvyMan4ZfjUj1VqI1ubPCsh-krpPI92Pm3z5zjs";
+            DocumentsResource.GetRequest request = service.Documents.Get(documentId);
+
+            // Parse the document's body 
+            Document doc = request.Execute();
+            Console.WriteLine($"Title: {doc.Title}");
+            var paragraphs = 0;
+            var days = 0;
+            var wfh = 0;
+            var tagList = new List<string>();
+            foreach (var element in doc.Body.Content)
+            {
+                if (element.Paragraph != null)
+                {
+                    paragraphs += 1;
+                    var sb = new StringBuilder();
+                    foreach (var parel in element.Paragraph.Elements)
+                    {
+                        sb.Append(parel.TextRun?.Content);
+                    }
+                    var text = sb.ToString();
+
+                    // Date "title" parsing
+                    if (IsADateHeading(text))
+                    {
+                        Console.WriteLine($"Fecha: {text.Trim()}");
+                        days += 1;
+                        if (text.ToLower().Contains("[wfh]")) wfh += 1;
+                        SearchForPossibleDayTags(text, tagList);
+                    }
+
+                    // Pluralsight example
+                    if ((text != null) && (text.Contains("Pluralsight"))) Console.WriteLine(text);
+                }
+            }
+            Console.WriteLine($"\nParagraph count: {paragraphs}");
+            Console.WriteLine($"Work days: {days}");
+            Console.WriteLine($"Remoting days: {wfh}");
+            Console.WriteLine("\nDay Tags:");
+            foreach (var item in tagList.OrderBy(t => t))
+            {
+                Console.WriteLine($"[{item}]");
+            }
+            Console.ReadLine();
+        }
+
+        private static void SearchForPossibleDayTags(string text, List<string> list)
+        {
+            var tags = new List<string>();
+            var bInsideTag = false;
+            var currentTag = "";
+            foreach (var c in text.ToCharArray())
+            {
+                switch (c)
+                {
+                    case '[':
+                        bInsideTag = true;
+                        break;
+                    case ']':
+                        bInsideTag = false;
+                        currentTag = currentTag.Trim();
+                        if (!tags.Contains(currentTag)) tags.Add(currentTag);
+                        currentTag = "";
+                        break;
+                    default:
+                        if (bInsideTag) currentTag += c.ToString();
+                        break;
+                }
+            }
+            foreach (var tag in tags)
+            {
+                if (!list.Contains(tag)) list.Add(tag);
+            }
+        }
+
+        private static bool IsADateHeading(string text)
+        {
+            if (text == null) return false;
+            return Regex.IsMatch(text.Trim(), @"^([2-9]\d{3}((0[1-9]|1[012])(0[1-9]|1\d|2[0-8])|(0[13456789]|1[012])(29|30)|(0[13578]|1[02])31)|(([2-9]\d)(0[48]|[2468][048]|[13579][26])|(([2468][048]|[3579][26])00))0229)\b");
+        }
+    }
+}
